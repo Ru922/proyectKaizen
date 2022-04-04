1a33e53ea80628e86a614727bcca1292064b7415
Ruu92 <rubenbaronetto11@gmail.com>
2022-04-04 10:05:12 -0300

    Puse las rutas en la carpeta routes

--- app.js
+++ app.js
@@ -21,4 +21,4 @@ app.get('/profesores/materias', (req,res) => {
 });
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log ('Escuchando desde el puerto 3000'));
+app.listen(PORT, () => console.log (`Escuchando desde el puerto ${PORT}`));
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/profesores/materias", (req,res)=>{
-
+router.get('/', (req,res) => {
+    res.send('Pagina de inicio')
 });
+
+
+router.get('/profesores/materias', (req,res) => {
+    res.send('Agregado de materias')
+});
+
 
 module.exports = router
